d558a7e1a6a6ef1ae9415c11c3b19201063be3bb
ThePuzzlers <damian@thepuzzlers.co>
2017-11-10 10:37:09 +0100

    implemented frame, image, text in slider

--- js/slider.js
+++ js/slider.js
@@ -3,27 +3,42 @@ const slider = document.getElementsByClassName('slider');
 const menuOne = document.getElementsByClassName('menu-one');
 const menuTwo = document.getElementsByClassName('menu-two');
 const menuThree = document.getElementsByClassName('menu-three');
+const germany = document.getElementsByClassName('germany');
 
-function openCard(event) {
+const product = document.getElementById('pear');
 
-    console.log(event);
-    
-    if (event.target.className === "menu-one") {
 
+
+slider['0'].addEventListener('click', event => {
+    if (event.target.className || event.target.parentElement.className || event.target.parentElement.parentElement.className === "menu-one") {
         menuOne['0'].style.width = '70%';
         menuTwo['0'].style.width = '15%';
         menuThree['0'].style.width = '15%';
+        pear.style.marginLeft = '10rem';
+        pear.style.transition = 'margin-left 1s ease-out';
+        germany['0'].style.height = '25rem';
+        germany['0'].style.transition = 'height 1s ease-out';  
+        console.log(event);
         
-    } else if (event.target.className === "menu-two") {
-
+    
+    } else if (event.target.className || event.target.parentElement.className || event.target.parentElement.parentElement.className === "menu-two") {
         menuOne['0'].style.width = '15%';
         menuTwo['0'].style.width = '70%';
         menuThree['0'].style.width = '15%';
-
-    } else {
-        
+        pear.style.marginLeft = '1rem';
+        pear.style.transition = 'margin-left 1s ease-out';
+        germany['0'].style.height = '18rem';
+        germany['0'].style.transition = 'height 1s ease-out';
+    } else if (event.target.className || event.target.parentElement.className || event.target.parentElement.parentElement.className === "menu-three") {
         menuOne['0'].style.width = '15%';
         menuTwo['0'].style.width = '15%';
         menuThree['0'].style.width = '70%';
     }
-}
+});
+
+
+// function checkCard(event) {
+//     console.log(event.path.indexOf('section.menu-one'));
+//     console.log(event);
+    
+// }
